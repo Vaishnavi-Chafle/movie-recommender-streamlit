e9c346c4efa818c47e97c5f6812d4427a6072f78
Vaishnavi Asok Chafle <vaishnavi.chafle24@gmail.com>
2025-06-29 19:06:57 +0530

    app.py

--- app.py
+++ app.py
@@ -58,14 +58,16 @@ st.title('🎬 Movie Recommender System with Posters')
 
 # Function to fetch movie poster from OMDb API
 def fetch_poster_omdb(movie_title):
-    api_key = "76a82709"  # Replace with your actual OMDb API key
+    api_key = st.secrets["OMDB_API_KEY"]
     url = f"http://www.omdbapi.com/?t={movie_title}&apikey={api_key}"
     response = requests.get(url)
     data = response.json()
+
     if data["Response"] == "True" and data["Poster"] != "N/A":
         return data["Poster"]
     else:
         return "https://via.placeholder.com/300x450.png?text=Poster+Not+Available"
+
 
 # Recommendation logic
 def recommend(movie):
